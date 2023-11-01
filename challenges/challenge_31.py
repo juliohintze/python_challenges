e7972651ecf3794b624ffa14b9cb8559ba45cbd5
Júlio Hintze <juliohintze@gmail.com>
2023-11-01 10:53:37 -0300

    Added challenge #31

---
+++ challenges/challenge_31.py
@@ -0,0 +1,5 @@
+# FizzBuzz challenge
+# Print the numbers from 1 to 100 (including 100), but:
+# - If the number is divisible by 3, instead of printing the number, print the word "Fizz"
+# - If the number is divisible by 5, instead of printing the number, print the word "Buzz"
+# - If the number is divisible by both 3 AND 5, instead of printing the number, print the word "FizzBuzz"
