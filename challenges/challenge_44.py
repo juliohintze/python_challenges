3952b6c965d33da21234495f814bc4f797da468c
Júlio Hintze <juliohintze@gmail.com>
2023-11-07 11:48:15 -0300

    Added challenge 44

---
+++ challenges/challenge_44.py
@@ -0,0 +1,3 @@
+# Guessing game! Choose a number from 1 to 100. Then, in a while loop, ask the user to guess the number.
+# When the user fails, give him a hint (if the given number is greater or lower than the secret number).
+# When the user wins, end the program.
