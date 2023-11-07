2ece81e69cdb779750ae86947716e6677936e9a3
Júlio Hintze <juliohintze@gmail.com>
2023-11-07 11:19:17 -0300

    Added challenges 42 and 43

---
+++ challenges/challenge_42.py
@@ -0,0 +1,1 @@
+# Get a text and a number (n) from the user. Then print the text (n) times using a While loop.
---
+++ challenges/challenge_43.py
@@ -0,0 +1,1 @@
+# Get a number from the user and then, using a While loop, print the multiplication table of that number.
