58324b1987541b1e44a96180cbf5d9f31ec0ffd1
Júlio Hintze <juliohintze@gmail.com>
2023-11-17 13:56:14 -0300

    Created challenge 56

---
+++ challenges/challenge_56.py
@@ -0,0 +1,6 @@
+# Create a BankAccount class. It should have a account number and balance.
+# It should also be possible to deposit and withdraw money from it.
+# Every time the user deposits money, the balance increases.
+# Every time the user withdraws money, the balance decreases.
+# Then create an object from that class. Deposit some money to it and
+# then print its balance.
